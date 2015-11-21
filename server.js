b5a82270e22c7cdeaa57ae1097db3fa88e8a3110
cglangan78 <cglangan78@gmail.com>
2015-11-20 16:44:48 -0800

    create user-controller and connect to user-routes, update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   port = process.env.PORT || 3000,
-  databaseLocal = 'mongodb://localhost:3000/bundlr-db'
-  database = 'mongodb://tester:tester@ds041613.mongolab.com:41613/bundlr-db'
-
+  databaseLocal = 'mongodb://localhost:3000/bundlr-db',
+  database = 'mongodb://tester:tester@ds041613.mongolab.com:41613/bundlr-db',
+  itemRoutes  = require('./routes/user_routes.js'),
   app = express();
 
 //establishes connection to MongoDB
@@ -19,6 +19,8 @@ mongoose.connect(databaseLocal, function(){
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/user', userRoutes);
 
 //start the server
 app.listen(port, function(){
