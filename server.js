f83cb85989dcfbd2d7e3105955d2be87171ca8f6
cglangan78 <cglangan78@gmail.com>
2015-11-20 16:03:23 -0800

    add middleware, database(mongolab)

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ var
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   port = process.env.PORT || 3000,
+  database = 'mongodb://localhost:3000/bundlr-db'
+  //database = 'mongodb://tester:tester@ds041613.mongolab.com:41613/bundlr-db'
+
   app = express();
 
+//establishes connection to MongoDB
+mongoose.connect(database);
+
+//middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 //start the server
 app.listen(port, function(){
