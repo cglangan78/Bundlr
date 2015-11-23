548ee675c15355125f9f600db6c4708a593a2e33
cglangan78 <cglangan78@gmail.com>
2015-11-23 09:29:44 -0800

    add youtube model, route, ajax, and basic html

--- public/app.js
+++ public/app.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+  var results = $('.results');
+  console.log(results)
 
+
+$(results).on('click', function(){
+  $.ajax({
+    url: '/youtube/popular',
+    method: 'GET',
+    contentType: 'application/json',
+    success: function(data){
+      console.log(data)
+      data.forEach(function(url){
+        var li = '<li>' + url.youtube_url + url._id + '</li>'
+        $('.results').append(li);
+      });
+    }
+   })
+  })
+})
---
+++ routes/youtube_routes.js
@@ -0,0 +1,31 @@
+var
+  express = require('express'),
+  request = require('request'),
+  youtubeRoutes = express.Router();
+  youtube = require('../models/youtube.js');
+
+
+youtubeRoutes.get('/popular', function(req,res){
+  // var q = $('#query').val('cat');
+  var key = 'AIzaSyCb8JsJ1jSYDDz9PihwYVgTJyiTTYaNpAw'
+  var requestUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&order=relevance&q=obama&key=" + key;
+
+  request(requestUrl, function(error, response, body) {
+    var bodyArr = JSON.parse(body).items;
+    for (var i = 0; i < bodyArr.length; i +=1) {
+      var videoId = bodyArr[i].id.videoId;
+      var url = 'https://www.youtube.com/watch?v=' + videoId
+        console.log(url)
+      var popularYoutubeURL = youtube({
+		       youtube_url: url
+	    })
+        popularYoutubeURL.save(function(err){
+   			if(err) throw err;
+      })
+    }
+    res.json(popularYoutubeURL)
+  })
+})
+
+
+module.exports = youtubeRoutes
