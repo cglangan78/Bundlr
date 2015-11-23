4ce4922b3378db40254609dc2d1ccca6933472a5
cglangan78 <cglangan78@gmail.com>
2015-11-23 11:36:15 -0800

    search displays 10 new urls, moved url request to frontend

--- public/app.js
+++ public/app.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
-  var submitbtn = $('#submit-btn')
-  var searchForm = $('#search-form')
-  console.log(submitbtn)
+  var $searchForm = $('#search-form');
+  var $results = $('.results');
 
-  searchForm.on('submit', function(evt){
-    evt.preventDefault()
-    console.log('clicked')
+  function clearResults(){
+      $results.html('');
+  }
+
+  $searchForm.on('submit', function(evt){
+    evt.preventDefault();
+    var $q = $('#search').val();
+    var key = 'AIzaSyCb8JsJ1jSYDDz9PihwYVgTJyiTTYaNpAw';
+    var requestUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=' + $q + '&key=' + key;
+    clearResults();
     $.ajax({
-      url: '/youtube/popular',
       method: 'GET',
+      url: requestUrl,
       contentType: 'application/json',
       success: function(data){
         console.log(data)
         var url = 'https://www.youtube.com/watch?v='
-        data.forEach(function(video){
+        data.items.forEach(function(video){
           var li = '<li>' + url + video.id.videoId + '</li>'
-          $('.results').append(li)
+          $results.append(li)
         });
       }
      })
