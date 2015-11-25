24a45c0d68e39637c9ad8b9b69c1bdb0ea96fd43
cglangan78 <cglangan78@gmail.com>
2015-11-25 15:24:36 -0800

    make changes to env

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'facebookAuth': {
-    'clientID': '1660243580854959',
-    'clientSecret': '3b769ca4ae936c78b3ac652ddeb73e99',
+    'clientID': '996724753700194',
+    'clientSecret': '994a8aab8c9a7e270c564fae3d64c170',
     'callbackURL': 'http://localhost:3000/auth/facebook/callback',
     'profileFields': ['emails', 'displayName']
   }
