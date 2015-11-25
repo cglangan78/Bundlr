56e9c80f7d6351c4434628a52ea28b0ab9895ff2
cglangan78 <cglangan78@gmail.com>
2015-11-25 15:41:48 -0800

    testing env

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   'facebookAuth': {
     'clientID': '996724753700194',
     'clientSecret': '994a8aab8c9a7e270c564fae3d64c170',
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+    'callbackURL': 'https://bundlr-rgk.herokuapp.com/auth/facebook/callback',
     'profileFields': ['emails', 'displayName']
   }
 }
