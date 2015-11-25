00a409325dadbf9f5ed9368d7ffa1e0481133191
Kevin <kevinjustinwilliams@gmail.com>
2015-11-24 20:22:27 -0800

    adjusted passport

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ var
     passport        = require('passport'),
     LocalStrategy   = require('passport-local').Strategy;
 
-var User            = require('../models/user.js');
+var User = require('../models/user.js');
 
 
 passport.serializeUser(function(user, done){
@@ -16,18 +16,16 @@ passport.deserializeUser(function(id, done){
 })
 
 passport.use('local-signup', new LocalStrategy({
-    firstnameField: 'firstName',
-    lastnameField: 'lastName',
-    emailField: 'email',
+    usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-}, function(req, firstName, lastName, email, password, done){
+}, function(req, email, password, done){
     User.findOne({'local.email': email}, function(err, user){
         if(err) return done(err)
         if(user) return done(null, false, req.flash('signupMessage','That email is already taken'))
 
         var newUser = new User()
-        newUser.local.firstName = req.body.firstname
+        newUser.local.firstName = req.body.firstName
         newUser.local.lastName = req.body.lastName
         newUser.local.email = email
         newUser.local.password = newUser.generateHash(password)
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(express.static('public'));
 
 app.use(session({
   secret: 'Richlaptop',
