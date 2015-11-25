dd5906af0e28b916fbcbb87142c67a986fa33f4b
cglangan78 <cglangan78@gmail.com>
2015-11-25 15:33:50 -0800

    testing env

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var
   session = require('express-session'),
   passport = require('passport'),
 	passportConfig = require('./config/passport.js'),
-  configAuth = require('./auth.js'),
+  configAuth = require('./config/auth.js'),
   app = express();
 
 //establishes connection to MongoDB
